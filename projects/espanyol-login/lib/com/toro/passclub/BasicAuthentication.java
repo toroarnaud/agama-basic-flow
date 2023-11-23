413946dd0b24e75c2768384ef65eee4e66e709ed
toroarnaud <arnaud@toro-intl.com>
2023-11-23 16:36:54 +0100

    feat: update BasicAuthentication.java

--- projects/espanyol-login/lib/com/toro/passclub/BasicAuthentication.java
+++ projects/espanyol-login/lib/com/toro/passclub/BasicAuthentication.java
@@ -8,18 +8,18 @@ import org.slf4j.LoggerFactory;
 
 public class BasicAuthentication {
 
-    private static final Logger scriptLogger = LoggerFactory.getLogger(BasicAuthentication.class);
+    private static final Logger scriptLogger = LoggerFactory.getLogger(CustomScriptManager.java.class);
     private static AuthenticationService authenticationService = CdiUtil.bean(AuthenticationService.class);
 
     public static boolean authenticate(String username, String password) {
-
+        System.out.println("Looooooooooooooooooogs!");
         scriptLogger.info("Passclub BasicAuthentication. authenticating with user: " + username);
         return authenticationService.authenticate("idm", password);
 
     }
 
     public static void testNoArg() {
-
+        System.out.println("Looooooooooooooooooogs!");
         scriptLogger.info("Passclub BasicAuthentication. Called with no arguments");
 
     }
