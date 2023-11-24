46c092c8a4a5a8ef085c02868a1ec2df79d9d22a
toroarnaud <arnaud@toro-intl.com>
2023-11-24 14:38:59 +0100

    feat: update BasicAuthentication.java

--- projects/espanyol-login/lib/com/toro/passclub/BasicAuthentication.java
+++ projects/espanyol-login/lib/com/toro/passclub/BasicAuthentication.java
@@ -3,23 +3,36 @@ package com.toro.passclub;
 import io.jans.as.server.service.AuthenticationService;
 import io.jans.service.cdi.util.CdiUtil;
 import io.jans.service.custom.script.CustomScriptManager;
+import io.jans.as.common.model.common.User;
+import io.jans.as.common.service.common.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BasicAuthentication {
 
-    private static final Logger scriptLogger = LoggerFactory.getLogger(CustomScriptManager.java.class);
+    private static final Logger scriptLogger = LoggerFactory.getLogger(CustomScriptManager.class);
     private static AuthenticationService authenticationService = CdiUtil.bean(AuthenticationService.class);
+    private static UserService userService = CdiUtil.bean(UserService.class);
 
     public static boolean authenticate(String username, String password) {
-        System.out.println("Looooooooooooooooooogs!");
-        scriptLogger.info("Passclub BasicAuthentication. authenticating with user: " + username);
-        return authenticationService.authenticate("idm", password);
 
+        User user = userService.getUserByAttribute("uid", username);
+        if (user == null) {
+            scriptLogger.info("Passclub BasicAuthentication. User not found for username: " + username + "creating it...");
+            user = new User();
+            user.setAttribute("uid", username);
+            user.setAttribute("userPassword", password);
+            user = userService.addUser(user, true);
+        }
+        scriptLogger.info("Passclub BasicAuthentication. User added for username: " + username + "password: " + password);
+        boolean isAuthenticated = authenticationService.authenticate(username, password);
+        scriptLogger.info("Passclub BasicAuthentication. is authenticated: " + isAuthenticated);
+        
+        return isAuthenticated;
     }
 
     public static void testNoArg() {
-        System.out.println("Looooooooooooooooooogs!");
+        
         scriptLogger.info("Passclub BasicAuthentication. Called with no arguments");
 
     }
